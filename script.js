cd1269bc3f92d1ca7af490522c9261649bb4e426
synthaxme <emanuelvidalrm@gmail.com>
2022-08-11 15:58:25 -0300

    second commit (added functions)

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function teste(){
     var resultado = document.getElementById("resultado");
     var resultado_imc = document.getElementById("resultado_imc");
+    var resultado_comment = document.getElementById("resultado_comment")
     var peso = document.getElementById("peso").value;
     var altura = document.getElementById("altura").value;
     const imc = Math.round(peso / (Math.pow(altura, 2)));
@@ -10,10 +11,12 @@ function teste(){
     if (imc > 30){
         resultado_imc.style.color = 'white';
         resultado_imc.style.backgroundColor = 'red'
+        resultado_comment.innerHTML = "Converse com seu médico, procure diminuir seu IMC! 👎🏼"
     }
     else{
         resultado_imc.style.color = 'white';
         resultado_imc.style.backgroundColor = 'green';
+        resultado_comment.innerHTML = "Seu IMC está sob controle! 👍🏼"
     }
 }
 
